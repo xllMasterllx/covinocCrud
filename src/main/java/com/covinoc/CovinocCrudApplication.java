99c9cc2f8a12aa62ef94bc20c48b7f92cf2d753f
jcasmont <jbreiner.castro@outlook.com>
2023-10-11 08:27:20 -0500

    Crear proyecto
    Se crea proyecto spring boot y sus dependencias 11/10/2023  08:26

---
+++ src/main/java/com/covinoc/CovinocCrudApplication.java
@@ -0,0 +1,13 @@
+package com.covinoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovinocCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovinocCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covinoc/CovinocCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covinoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovinocCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
